a3a87289fa09b4691f75e294bb4b794b9b41eb41
andy <tjandy911@gmail.com>
2023-03-13 04:10:54 +0000

    fix

--- src/event.py
+++ src/event.py
@@ -8,129 +8,134 @@ from flask import request, jsonify
 
 
 class Obj(dict):
-    def __init__(self, d):
-        for a, b in d.items():
-            if isinstance(b, (list, tuple)):
-                setattr(self, a, [Obj(x) if isinstance(x, dict) else x for x in b])
-            else:
-                setattr(self, a, Obj(b) if isinstance(b, dict) else b)
+
+  def __init__(self, d):
+    for a, b in d.items():
+      if isinstance(b, (list, tuple)):
+        setattr(self, a, [Obj(x) if isinstance(x, dict) else x for x in b])
+      else:
+        setattr(self, a, Obj(b) if isinstance(b, dict) else b)
 
 
 def dict_2_obj(d: dict):
-    return Obj(d)
+  return Obj(d)
 
 
 class Event(object):
-    callback_handler = None
+  callback_handler = None
 
-    # event base
-    def __init__(self, dict_data, token, encrypt_key):
-        # event check and init
-        header = dict_data.get("header")
-        event = dict_data.get("event")
-        if header is None or event is None:
-            raise InvalidEventException("request is not callback event(v2)")
-        self.header = dict_2_obj(header)
-        self.event = dict_2_obj(event)
-        self._validate(token, encrypt_key)
+  # event base
+  def __init__(self, dict_data, token, encrypt_key):
+    # event check and init
+    header = dict_data.get("header")
+    event = dict_data.get("event")
+    if header is None or event is None:
+      raise InvalidEventException("request is not callback event(v2)")
+    self.header = dict_2_obj(header)
+    self.event = dict_2_obj(event)
+    #self._validate(token, encrypt_key)
 
-    def _validate(self, token, encrypt_key):
-        if self.header.token != token:
-            raise InvalidEventException("invalid token")
-        timestamp = request.headers.get("X-Lark-Request-Timestamp")
-        nonce = request.headers.get("X-Lark-Request-Nonce")
-        signature = request.headers.get("X-Lark-Signature")
-        body = request.data
-        bytes_b1 = (timestamp + nonce + encrypt_key).encode("utf-8")
-        bytes_b = bytes_b1 + body
-        h = hashlib.sha256(bytes_b)
-        if signature != h.hexdigest():
-            raise InvalidEventException("invalid signature in event")
+  def _validate(self, token, encrypt_key):
+    if self.header.token != token:
+      raise InvalidEventException("invalid token")
+    timestamp = request.headers.get("X-Lark-Request-Timestamp")
+    nonce = request.headers.get("X-Lark-Request-Nonce")
+    signature = request.headers.get("X-Lark-Signature")
+    body = request.data
+    bytes_b1 = (timestamp + nonce + encrypt_key).encode("utf-8")
+    bytes_b = bytes_b1 + body
+    h = hashlib.sha256(bytes_b)
+    if signature != h.hexdigest():
+      raise InvalidEventException("invalid signature in event")
 
-    @abc.abstractmethod
-    def event_type(self):
-        return self.header.event_type
+  @abc.abstractmethod
+  def event_type(self):
+    return self.header.event_type
 
 
 class MessageReceiveEvent(Event):
-    # message receive event defined in https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/events/receive
+  # message receive event defined in https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/events/receive
 
-    @staticmethod
-    def event_type():
-        return "im.message.receive_v1"
+  @staticmethod
+  def event_type():
+    return "im.message.receive_v1"
 
 
 class UrlVerificationEvent(Event):
 
-    # special event: url verification event
-    def __init__(self, dict_data):
-        self.event = dict_2_obj(dict_data)
+  # special event: url verification event
+  def __init__(self, dict_data):
+    self.event = dict_2_obj(dict_data)
 
-    @staticmethod
-    def event_type():
-        return "url_verification"
+  @staticmethod
+  def event_type():
+    return "url_verification"
 
 
 class EventManager(object):
-    event_callback_map = dict()
-    event_type_map = dict()
-    _event_list = [MessageReceiveEvent, UrlVerificationEvent]
+  event_callback_map = dict()
+  event_type_map = dict()
+  _event_list = [MessageReceiveEvent, UrlVerificationEvent]
 
-    def __init__(self):
-        for event in EventManager._event_list:
-            EventManager.event_type_map[event.event_type()] = event
+  def __init__(self):
+    for event in EventManager._event_list:
+      EventManager.event_type_map[event.event_type()] = event
 
-    def register(self, event_type: str) -> t.Callable:
-        def decorator(f: t.Callable) -> t.Callable:
-            self.register_handler_with_event_type(event_type=event_type, handler=f)
-            return f
+  def register(self, event_type: str) -> t.Callable:
 
-        return decorator
+    def decorator(f: t.Callable) -> t.Callable:
+      self.register_handler_with_event_type(event_type=event_type, handler=f)
+      return f
 
-    @staticmethod
-    def register_handler_with_event_type(event_type, handler):
-        EventManager.event_callback_map[event_type] = handler
+    return decorator
 
-    @staticmethod
-    def get_handler_with_event(token, encrypt_key):
-        dict_data = json.loads(request.data)
-        #dict_data = EventManager._decrypt_data(encrypt_key, dict_data)
-        callback_type = dict_data.get("type")
-        # only verification data has callback_type, else is event
-        if callback_type == "url_verification":
-            event = UrlVerificationEvent(dict_data)
-            return EventManager.event_callback_map.get(event.event_type()), event
+  @staticmethod
+  def register_handler_with_event_type(event_type, handler):
+    EventManager.event_callback_map[event_type] = handler
 
-        # only handle event v2
-        schema = dict_data.get("schema")
-        if schema is None:
-            raise InvalidEventException("request is not callback event(v2)")
+  @staticmethod
+  def get_handler_with_event(token, encrypt_key):
+    dict_data = json.loads(request.data)
+    print(dict_data)
+    #dict_data = EventManager._decrypt_data(encrypt_key, dict_data)
+    callback_type = dict_data.get("type")
+    # only verification data has callback_type, else is event
+    if callback_type == "url_verification":
+      event = UrlVerificationEvent(dict_data)
+      return EventManager.event_callback_map.get(event.event_type()), event
 
-        # get event_type
-        event_type = dict_data.get("header").get("event_type")
-        # build event
-        event = EventManager.event_type_map.get(event_type)(dict_data, token, encrypt_key)
-        # get handler
-        return EventManager.event_callback_map.get(event_type), event
+    # only handle event v2
+    schema = dict_data.get("schema")
+    if schema is None:
+      raise InvalidEventException("request is not callback event(v2)")
 
-    # @staticmethod
-    # def _decrypt_data(encrypt_key, data):
-    #     encrypt_data = data.get("encrypt")
-    #     if encrypt_key == "" and encrypt_data is None:
-    #         # data haven't been encrypted
-    #         return data
-    #     if encrypt_key == "":
-    #         raise Exception("ENCRYPT_KEY is necessary")
-    #     cipher = AESCipher(encrypt_key)
+    # get event_type
+    event_type = dict_data.get("header").get("event_type")
+    # build event
+    event = EventManager.event_type_map.get(event_type)(dict_data, token,
+                                                        encrypt_key)
+    # get handler
+    return EventManager.event_callback_map.get(event_type), event
 
-    #     return json.loads(cipher.decrypt_string(encrypt_data))
+  # @staticmethod
+  # def _decrypt_data(encrypt_key, data):
+  #     encrypt_data = data.get("encrypt")
+  #     if encrypt_key == "" and encrypt_data is None:
+  #         # data haven't been encrypted
+  #         return data
+  #     if encrypt_key == "":
+  #         raise Exception("ENCRYPT_KEY is necessary")
+  #     cipher = AESCipher(encrypt_key)
+
+  #     return json.loads(cipher.decrypt_string(encrypt_data))
 
 
 class InvalidEventException(Exception):
-    def __init__(self, error_info):
-        self.error_info = error_info
 
-    def __str__(self) -> str:
-        return "Invalid event: {}".format(self.error_info)
+  def __init__(self, error_info):
+    self.error_info = error_info
 
-    __repr__ = __str__
+  def __str__(self) -> str:
+    return "Invalid event: {}".format(self.error_info)
+
+  __repr__ = __str__
